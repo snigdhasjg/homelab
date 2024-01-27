43aeef0961c6ad06fa943988d6d76bb6dcdc7e4e
pi5 <snigdhasjg@users.noreply.github.com>
2024-01-28 04:13:52 +0530

    Update portainer start command

--- portainer-start.sh
+++ portainer-start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 docker stop portainer
 docker rm portainer
 
@@ -11,7 +12,7 @@ docker run -d \
     -v portainer_data:/data \
     -v /home/joe/.cert/docker.snigji.com:/certs:ro \
     portainer/portainer-ce:linux-arm64-2.19.4 \
-        --bind :80 \
+        --http-disabled \
         --bind-https :443 \
         --sslcert /certs/docker.snigji.com.crt \
         --sslkey /certs/docker.snigji.com.key
